94e57ab0c323772dbf34b12d02ed368c294983ce
evenp <Philippe.Even@loria.fr>
2022-11-08 22:52:14 +0100

    Test parameters explicited

--- src/test_parameters.py
+++ src/test_parameters.py
@@ -10,10 +10,11 @@ TEST_MODEL = "unet/unet-amrel-processed-grismouton"				# name of the model to ev
 #TEST_MODEL = "ddcm/ddcm-roads-5px-grismouton"			
 TEST_AREA = "grismouton"										# area to test the model on 
 TEST_INPUT_TYPES = ["slope"]									# image types to use as input during evaluation (N types will be concatenated into a single N channels image)
-TEST_MASK_TYPES = ["roads-5px", "roads-1px", "roads-28px"]		# ground truth used to compute metrics : 
+TEST_MASK_TYPES = ["roads-5px", "roads-1px", "roads-28px"]		# ground truth used to compute metrics (to compare with AMREL) : 
 																#	- 1st one is used to compute all metrics if USE_AMREL_METRICS is False
 																#	- 2nd one is used to compute recall if USE_AMREL_METRICS is True)
 																#	- 3rd one is used to compute precision if USE_AMREL_METRICS is True)
+#TEST_MASK_TYPES = ["amrel-processed", "amrel-1px", "amrel-28px"]	#to compare with different CNN architectures
 
 PREDICTION_ONLY = False											# relevant if no ground truth is available for the test area : if true, the metrics aren't computed and only the predicted binary mask will be saved
 SAVE_PREDICTION = True											# whether or not to save predictions as images
